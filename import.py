6226507545a76843f1230e674e7bcf2c74e371ba
Porky <75227240+PorkyC@users.noreply.github.com>
2022-11-11 12:23:56 -0800

    import rates from psql to json

--- import.py
+++ import.py
@@ -1,9 +1,24 @@
 import psycopg2
+import json
 
 conn = psycopg2.connect(dbname="fx_rates", user="postgres", password="postgres", host="localhost", port="5432")
 cursor = conn.cursor()
 
+f = open("rates.json")
+data = json.load(f)
+
+for row in data:
+    print(row)
+
 cursor.execute("SELECT * FROM boc_rates")
 rows = cursor.fetchall()
 for row in rows:
-    print(row)
+    d = row[0].strftime("%Y-%m-%d")
+    for i in range(1, len(row)):
+        currency = "FX" + data[i-1]["symbol"].upper() + "CAD"
+        v = row[i]
+        if v:
+            data[i-1]["rates"].append({"d": d, currency:{"v": str(v)}})
+
+w = open("out.json", "w")
+w.write(json.dumps(data, indent=4))

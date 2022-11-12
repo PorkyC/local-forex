4581cea66e787ba9dd61b5f168c78e4cc52498f1
Porky <75227240+PorkyC@users.noreply.github.com>
2022-11-11 17:13:01 -0800

    remove testing lines in init

--- local_forex/__init__.py
+++ local_forex/__init__.py
@@ -1,7 +1,3 @@
 from local_forex import ForexRates
 
 forex_dict = ForexRates()
-print(forex_dict.get_rate("uSd"))
-print(forex_dict.get_conversion_rate("CAD", "USD"))
-boc_rates_json = forex_dict.fetch_boc_rates()
-forex_dict.update_boc_rates(boc_rates_json)

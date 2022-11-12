0ab59ad770bf6bb8db1a4c5656247150bfce3114
Porky <75227240+PorkyC@users.noreply.github.com>
2022-11-11 17:11:33 -0800

    Comments and completed update function

--- local_forex/local_forex.py
+++ local_forex/local_forex.py
@@ -1,10 +1,43 @@
 import json
 from datetime import datetime, timedelta
+import requests
 
 class LocalForex:
     def __init__(self):
-        self.data = json.load(open("local_forex/rate_data.json", "r"))
+        self.data = self.load_rates_from_file()
         self.symbols = self.data.keys()
+    
+    # load_rates_from_file loads the data dictionary from a json file
+    def load_rates_from_file(self):
+        return json.load(open("local_forex/rate_data.json", "r"))
+    
+    # save_rates_to_file saves the data dictionary to a json file
+    def save_rates_to_file(self):
+        json.dump(self.data, open("local_forex/rate_data.json", "w"), indent=4)
+
+    # fetch_boc_rates returns the latest exchange rates in json format from the Bank of Canada website
+    def fetch_boc_rates(self):
+        # Fetch the latest exchange rates from the Bank of Canada website
+        url = "https://www.bankofcanada.ca/valet/observations/group/FX_RATES_DAILY/json"
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+
+    # update_boc_rates takes the json data from the Bank of Canada website and updates the rates in the data dictionary
+    def update_boc_rates(self, data):
+        up_to_date = False
+        i = -1
+        while not up_to_date:
+            date = data['observations'][i]['d']
+            for key, value in data['observations'][i].items():
+                if key != 'd':
+                    symbol = key.lower()[2:5]
+                    if self.data[symbol]['rates'].get(date) is None:
+                        self.data[symbol]['rates'][date] = str(value['v'])
+                        i += 1
+                    else:
+                        up_to_date = True
 
 class ForexRates(LocalForex):
     def get_name(self, symbol):
@@ -16,6 +49,8 @@ class ForexRates(LocalForex):
     def check_symbol(self, symbol):
         return True if symbol in self.symbols else False
 
+    # get_rate returns the rate given a symbol on a given date
+    # If a rate is not available on the given date, it will return the first available rate prior to that date 
     def get_rate(self, symbol, date=datetime.today()):
         symbol = symbol.lower()
         if not self.check_symbol(symbol):
@@ -29,11 +64,8 @@ class ForexRates(LocalForex):
         except KeyError:
             return self.get_rate(symbol, date - timedelta(days=1))
     
-    # get_rate returns the rate given a base/quote (quote defaults to cad) symbol pair on a given date
-    # If no date is given, the most recent rate is returned used
+    # get_conversion_rate returns the conversion rate between two symbols on a given date
     def get_conversion_rate(self, base, quote="CAD", date=datetime.today()):
         quote_rate = self.get_rate(quote, date)
         base_rate = self.get_rate(base, date)
         return base_rate / quote_rate
-
-    
